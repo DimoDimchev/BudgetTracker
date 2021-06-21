c9660aab42e2aea7ffab7fa4a18d0bd28d9e0a96
Dimo Dimchev <ddimchev7@gmail.com>
2021-06-21 23:25:34 +0300

    Added icons and customized colors of the navigation

--- routes/tabNavigation.js
+++ routes/tabNavigation.js
@@ -2,15 +2,45 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
 import HomeScreen from '../screens/homeScreen';
 import HistoryScreen from '../screens/historyScreen';
 
 const Tab = createBottomTabNavigator();
 
+// create tab navigation and customize the options(change colors and add icons)
 export default function TabNavigator() {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    // add an icon based on the route
+                    if (route.name === 'Home') {
+                        return (
+                            <Ionicons
+                                name={focused ? 'home' : 'home-outline'}
+                                size={size}
+                                color={color}
+                            />
+                        );
+                    } else if (route.name === 'History') {
+                        return (
+                            <Ionicons
+                                name={focused ? 'calendar' : 'calendar-outline'}
+                                size={size}
+                                color={color}
+                            />
+                        );
+                    }
+                },
+            })}
+                // customize icon color
+                tabBarOptions={{
+                    activeTintColor: '#145ac4',
+                    inactiveTintColor: 'gray',
+                }}
+            >
                 <Tab.Screen name="Home" component={HomeScreen} />
                 <Tab.Screen name="History" component={HistoryScreen} />
             </Tab.Navigator>
