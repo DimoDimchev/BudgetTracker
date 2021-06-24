2c0b4b96d553f476d3ee4f05c740bfcf1beca7df
Dimo Dimchev <ddimchev7@gmail.com>
2021-06-24 22:01:05 +0300

    Did the styling of the home screen

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -6,7 +6,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>This is the home screen!</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>Welcome to your favorite budget tracker!</Text>
+        <Text style={styles.paragraph}>This is the home screen. Here you can see information about the current month.</Text>
+      </View>
+      <View style={styles.subContainer}>
+        <Text style={styles.subtitle}>Total spendings:</Text>
+      </View>
+      <View style={styles.subContainer}>
+        <Text style={styles.subtitle}>Spendings by category:</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,7 +25,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginHorizontal: 20,
+    marginTop: 80,
+    paddingBottom: 20,
+  },
+  paragraph:{
+    paddingBottom: 20,
+    marginHorizontal: 20,
+    fontSize: 16,
+  },
+  subtitle: {
+    marginHorizontal: 20,
+    marginTop: 20,
+    marginBottom: 10,
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+  subContainer: {
+    width: 250,
+    borderBottomColor: '#b8b8b8',
+    borderBottomWidth: 1,
+    borderBottomStartRadius: 20,
+    borderBottomEndRadius: 20, 
+  },
+  header: {
+    backgroundColor: '#daffc9',
   },
 });
