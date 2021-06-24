dc48a2ad0e9906408f996632bc1f6663acc5f734
Dimo Dimchev <ddimchev7@gmail.com>
2021-06-24 22:37:09 +0300

    Created function to fetch total spendings  for current month

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, FlatList, View } from 'react-native';
+
+// base URI for the API
+const baseURI = 'https://react-native-budget-tracker-default-rtdb.europe-west1.firebasedatabase.app/';
 
 // create screen to keep track of the current month's spendings + option to add new expense
 export default function HomeScreen() {
+  let currentMonth = new Date();
+  currentMonth = currentMonth.getMonth().toString();
+
+  // function to get the total amount of spendings from this month
+  const getTotalSpendings = () => {
+    fetch(baseURI + currentMonth + '.json')
+      .then(res => res.json())
+      .then(data => {
+        let month = Object.keys(data).map(key => {
+          return { key: key, total: data[key]["total"] }
+        });
+        setTotalSpendings(month[0].total);
+      })
+  };
+
+  // function to get all of the categories for this month
+  const getCategories = () => {
+    return undefined;
+  };
+
+  const [totalSpendings, setTotalSpendings] = useState(getTotalSpendings);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.title}>Welcome to your favorite budget tracker!</Text>
         <Text style={styles.paragraph}>This is the home screen. Here you can see information about the current month.</Text>
       </View>
-      <View style={styles.subContainer}>
-        <Text style={styles.subtitle}>Total spendings:</Text>
-      </View>
-      <View style={styles.subContainer}>
+      <View>
+        <Text style={styles.subtitle}>Total spendings this month: {totalSpendings}</Text>
         <Text style={styles.subtitle}>Spendings by category:</Text>
+        <FlatList
+
+        />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -33,7 +59,7 @@ const styles = StyleSheet.create({
     marginTop: 80,
     paddingBottom: 20,
   },
-  paragraph:{
+  paragraph: {
     paddingBottom: 20,
     marginHorizontal: 20,
     fontSize: 16,
@@ -44,13 +70,6 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     fontSize: 17,
     fontWeight: 'bold',
-  },
-  subContainer: {
-    width: 250,
-    borderBottomColor: '#b8b8b8',
-    borderBottomWidth: 1,
-    borderBottomStartRadius: 20,
-    borderBottomEndRadius: 20, 
   },
   header: {
     backgroundColor: '#daffc9',
