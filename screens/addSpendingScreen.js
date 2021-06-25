7a7e07e5c3c6fe91c5c3bd11d304e57eac61c0f0
Dimo Dimchev <ddimchev7@gmail.com>
2021-06-25 13:06:40 +0300

    Keyboard now dismisses when clicking out of form

--- screens/addSpendingScreen.js
+++ screens/addSpendingScreen.js
@@ -1,33 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableWithoutFeedback, Keyboard } from 'react-native';
 
 // create screen where users can create a new spending
 export default function addSpendingScreen() {
     return (
         <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.title}>Add a new spending right here!</Text>
-                <Text style={styles.paragraph}>This is the New Spending screen. Here you can add a new expense, set its value and assign a category to it.</Text>
-            </View>
-            <View style={styles.formContainer}>
-                <Text>Title:</Text>
-                <TextInput
-                    placeholder="Write a title here"
-                    style={styles.input}
-                />
-                <Text>Amount spent:</Text>
-                <TextInput
-                    placeholder="Set an amount here"
-                    keyboardType="numeric"
-                    style={styles.input}
-                />
-                <Text>Category:</Text>
-                <TextInput
-                    placeholder="Set a category here:"
-                    style={styles.input}
-                />
-            </View>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View>
+                    <View style={styles.header}>
+                        <Text style={styles.title}>Add a new spending right here!</Text>
+                        <Text style={styles.paragraph}>This is the New Spending screen. Here you can add a new expense, set its value and assign a category to it.</Text>
+                    </View>
+                    <View style={styles.formContainer}>
+                        <Text>Title:</Text>
+                        <TextInput
+                            placeholder="Write a title here"
+                            style={styles.input}
+                        />
+                        <Text>Amount spent:</Text>
+                        <TextInput
+                            placeholder="Set an amount here"
+                            keyboardType="numeric"
+                            style={styles.input}
+                        />
+                        <Text>Category:</Text>
+                        <TextInput
+                            placeholder="Set a category here:"
+                            style={styles.input}
+                        />
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
         </View>
     );
 }
