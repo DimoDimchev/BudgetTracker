09598b1357ca12c75ab4428847e9917b044f089c
Dimo Dimchev <ddimchev7@gmail.com>
2021-06-27 19:51:37 +0300

    Removed unnecessary imports

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import TabNavigator from './routes/tabNavigation';
-import { NavigationContainer } from '@react-navigation/native';
 
 
 export default function App() {
   return (
-      <TabNavigator/>
+    <TabNavigator />
   );
 }
 
